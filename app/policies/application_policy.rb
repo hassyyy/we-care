a0707ea28d4fedc42dded25b61859af50020d4b8
hassyyy <nmohamedasan@gmail.com>
2022-07-06 16:48:23 +0530

    Disable update

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    create?
+    false
   end
 
   def edit?
--- app/policies/donation_policy.rb
+++ app/policies/donation_policy.rb
@@ -1,9 +1,5 @@
 class DonationPolicy < ApplicationPolicy
 
-  def edit?
-    false
-  end
-
   class Scope < Scope
     def resolve
       scope.all
