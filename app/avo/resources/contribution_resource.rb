a4770a6af5395efd7d4f9b7be8c052e33a767253
hassyyy <nmohamedasan@gmail.com>
2022-07-06 06:14:57 +0530

    Contributions - Single Column for Month & Date

--- app/avo/resources/contribution_resource.rb
+++ app/avo/resources/contribution_resource.rb
@@ -11,12 +11,12 @@ class ContributionResource < Avo::BaseResource
   field :value, as: :number, required: true, default: -> { context[:user].contributions.first&.value }
 
   field :month, as: :hidden, only_on: :forms, default: -> { Date.today.strftime("%b") }
-  field :month, as: :text, hide_on: :forms
-
   field :year, as: :hidden, only_on: :forms, default: -> { Date.today.year }
-  field :year, as: :number, hide_on: :forms
+  field :month, as: :text, only_on: :index do |model, resource, view|
+    "#{model.month} #{model.year}"
+  end
 
   field :status, as: :badge, options: { info: ['submitted'], success: 'sent' }
   field :status, as: :hidden, visible: -> (resource:) { !context[:user].account_admin? }, only_on: :forms, default: 'submitted'
-  field :status, as: :select, visible: -> (resource:) { context[:user].account_admin? }, only_on: :forms, required: true, options: { 'Submitted': :submitted, 'Sent': :sent }, default: 'submitted'  
+  field :status, as: :select, visible: -> (resource:) { context[:user].account_admin? }, only_on: :forms, required: true, options: { 'Submitted': :submitted, 'Sent': :sent }, default: 'submitted'
 end
