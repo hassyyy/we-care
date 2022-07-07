65bb16f7e4af071b125d75a6c0b2ff854f298bf8
hassyyy <nmohamedasan@gmail.com>
2022-07-07 21:25:41 +0530

    Change root path to home dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "avo/contributions#index"
+  root "avo/dashboards#show", id: 'home_dashboard'
 
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
