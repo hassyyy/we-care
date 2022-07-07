2042e943f55941bd78aa921b2dabdffdabaf80b0
hassyyy <nmohamedasan@gmail.com>
2022-07-07 19:46:02 +0530

    Delete scripts

--- scripts.rb
+++
@@ -1,2 +0,0 @@
-User.create!(name: "Hassyyy", email: "nmohamedasan@gmail.com", phone: 9043921046, password: "testtest", password_confirmation: "testtest", account_admin: true)
-User.create!(name: "Asan", email: "hassain13@gmail.com", phone: 9043921045, password: "testtest", password_confirmation: "testtest", account_admin: false)
