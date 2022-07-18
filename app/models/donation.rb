0f5a771871b5df00b048ce2d61efd968b8102fde
hassyyy <nmohamedasan@gmail.com>
2022-07-18 22:13:58 +0530

    Remove validations for donation value

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -3,12 +3,7 @@ class Donation < ApplicationRecord
   default_scope { order(created_at: :desc) }
 
   validates :resource_id, presence: true
-  validates :value, presence: true,
-                    numericality: {
-                                    only_integer: true,
-                                    greater_than: 0,
-                                    less_than_or_equal_to: 50000
-                                  }
+  validates :value, presence: true
   validates :description, presence: true
   validates :date, presence: true
 end
